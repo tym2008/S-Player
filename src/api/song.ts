ddcff4a7b52167610148c7743132887eb6a529ef
tym2008 <102407847+tym2008@users.noreply.github.com>
2024-12-05 13:30:11 +0800

    Update song.ts

--- src/api/song.ts
+++ src/api/song.ts
@@ -1,5 +1,8 @@
 import { songLevelData } from "@/utils/meta";
 import request from "@/utils/request";
+import { encryptQuery } from "./kwDES";
+import { SongUrlResult } from "./unblock";
+import axios from "axios";
 
 // 获取歌曲详情
 export const songDetail = (ids: number | number[]) => {
