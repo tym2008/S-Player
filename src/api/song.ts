194ee5271cfb7c0ed27649e58c4e9c3291a54f7e
tym2008 <102407847+tym2008@users.noreply.github.com>
2024-12-18 17:46:58 +0800

    Update song.ts
    301

--- src/api/song.ts
+++ src/api/song.ts
@@ -68,7 +68,7 @@ export const unlockSongUrl = async (
   if (server === "netease") {
     try {
       const response = await axios.get(
-        `https://gd-api.tym.us.kg/api.php?types=url&source=netease&id=${id}&br=128`
+        `https://301.tym.us.kg/https://music-api.gdstudio.xyz/api.php?types=url&source=netease&id=${id}&br=128`
       );
       const songUrl = response.data?.url;
       if (songUrl) {
