2e4957d53556f063b8adb9f94ce1c212068f8665
tym2008 <102407847+tym2008@users.noreply.github.com>
2025-08-08 17:16:15 +0800

    Update player.ts

--- src/utils/player.ts
+++ src/utils/player.ts
@@ -428,13 +428,9 @@ class Player {
     // 更新频谱数据
     const updateSpectrumData = () => {
       if (this.analyser && this.dataArray) {
-        /*
         this.analyser.getByteFrequencyData(this.dataArray);
         // 保存数据
-        statusStore.spectrumsData = Array.from(this.dataArray);*/
-        this.analyser.getByteFrequencyData(this.dataArray);
-        // 通过创建一个新的 Uint8Array 实例来解决类型冲突，并转换为普通数组
-        statusStore.spectrumsData = Array.from(new Uint8Array(this.dataArray));
+        statusStore.spectrumsData = Array.from(this.dataArray);
       }
       requestAnimationFrame(updateSpectrumData);
     };
